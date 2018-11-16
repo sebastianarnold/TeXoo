43b36a0fa14c7216e5031b164887d310fac97944
Sebastian Arnold <sarnold@beuth-hochschule.de>
2018-11-16 09:52:21 +0100

    Added texoo-annotate-ner script

--- texoo-entity-recognition/src/main/java/de/datexis/ner/exec/RunMentionAnnotator.java
+++ texoo-entity-recognition/src/main/java/de/datexis/ner/exec/RunMentionAnnotator.java
@@ -1,16 +1,18 @@
 package de.datexis.ner.exec;
 
 import de.datexis.common.CommandLineParser;
-import de.datexis.common.Configuration;
+import de.datexis.common.ObjectSerializer;
+import de.datexis.common.Resource;
+import de.datexis.model.Dataset;
+import de.datexis.model.Document;
+import de.datexis.ner.GenericMentionAnnotator;
+import de.datexis.ner.MentionAnnotator;
+import de.datexis.reader.RawTextDatasetReader;
 import java.io.IOException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.deeplearning4j.ui.api.UIServer;
-import org.nd4j.linalg.api.buffer.DataBuffer;
-import org.nd4j.linalg.api.buffer.util.DataTypeUtil;
-import org.nd4j.linalg.factory.Nd4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,6 +31,7 @@ public class RunMentionAnnotator {
     
     try {
       parser.parse(args);
+      new RunMentionAnnotator().runAnnotator(params);
     } catch(ParseException e) {
       HelpFormatter formatter = new HelpFormatter();
       formatter.printHelp("texoo-annotate-ner", "TeXoo: run pre-trained MentionAnnotator model", params.setUpCliOptions(), "", true);
@@ -39,28 +42,55 @@ public class RunMentionAnnotator {
   protected static class ExecParams implements CommandLineParser.Options {
 
     protected String inputFiles;
-    protected String seedList;
-    protected String outputPath = null; //"/home/sarnold/Library/Models/TeXoo/sector_2.0/SectorAnnotator_de_disease_SECTOR+bloom_20180515/head/";
-    protected boolean trainingUI = true;
+    protected String outputPath = null;
+    protected String language = null;
 
     @Override
     public void setParams(CommandLine parse) {
       inputFiles = parse.getOptionValue("i");
-      seedList = parse.getOptionValue("s");
       outputPath = parse.getOptionValue("o");
-      trainingUI = parse.hasOption("u");
+      //language = parse.getOptionValue("l");
     }
 
     @Override
     public Options setUpCliOptions() {
       Options op = new Options();
-      op.addRequiredOption("i", "input", true, "path and file name pattern for raw input text");
-      op.addRequiredOption("s", "seed", true, "path to seed list text file");
-      op.addRequiredOption("o", "output", true, "path to create and store the model");
-      op.addOption("u", "ui", false, "enable training UI");
+      op.addRequiredOption("i", "input", true, "path or file name for raw input text");
+      op.addOption("o", "output", true, "path to create and store the output JSON, otherwise dump to stdout");
+      //op.addOption("l", "language", true, "language to use for annotation (EN or DE)");
       return op;
     }
 
+  }
+  
+  protected void runAnnotator(ExecParams params) throws IOException {
+    
+    // Configure parameters
+    Resource inputPath = Resource.fromDirectory(params.inputFiles);
+    Resource outputPath = params.outputPath != null ? Resource.fromDirectory(params.outputPath) : null;
+    //WordHelpers.Language lang = WordHelpers.getLanguage(params.language);
+    
+    // Read datasets
+    // TODO: stream would be better so we can annotate file-by-file
+    Dataset data = new RawTextDatasetReader().read(inputPath);
+
+    // Load model
+    MentionAnnotator ner = GenericMentionAnnotator.create();
+
+    // Annotate
+    // TODO: skip existing extractions unless -f is specified
+    ner.annotate(data.getDocuments());
+    
+    // Save
+    if(params.outputPath != null) {
+      if(!outputPath.exists()) outputPath.toFile().mkdirs();
+      for(Document doc : data.getDocuments()) {
+        ObjectSerializer.writeJSON(doc, outputPath.resolve(doc.getId() + ".json"));
+      }
+    } else {
+      System.out.print(ObjectSerializer.getJSON(data));
+    }
+    
   }
   
 }
