86926d24a5989d2df746f6b3d4c3f0a99f324116
rudolf schneider <rudolf.schneider@beuth-hochschule.de>
2018-11-30 17:57:46 +0100

    Added Weighting terms. Started implementation of negative sampling.

--- texoo-core/src/main/java/de/datexis/loss/StructurePreservingEmbeddingLoss.java
+++ texoo-core/src/main/java/de/datexis/loss/StructurePreservingEmbeddingLoss.java
@@ -14,6 +14,16 @@ public class StructurePreservingEmbeddingLoss implements ILossFunction {
 
   private int margin;
   private int kNegativeExamples;
+  private int joinTerm2Weight; // lambda1
+  private int structureConstraintXWeight; // lambda2
+  private int structureConstrainYWeight; // lambda3
+
+  public StructurePreservingEmbeddingLoss() {
+    structureConstrainYWeight = 1;
+    structureConstraintXWeight = 1;
+    joinTerm2Weight = 1;
+    kNegativeExamples = 1;
+  }
 
   @Override
   public double computeScore(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask, boolean average) {
@@ -47,16 +57,15 @@ public class StructurePreservingEmbeddingLoss implements ILossFunction {
     INDArray y = scoreArr.get(NDArrayIndex.all(),NDArrayIndex.interval(2,4));
 
     INDArray yContrastive = Nd4j.create(y.shape());
-    yContrastive.putRow(0, y.getRow(1));
-    yContrastive.putRow(1, y.getRow(2));
-    yContrastive.putRow(2, y.getRow(3));
-    yContrastive.putRow(3, y.getRow(0));
-    
     INDArray xContrastive = Nd4j.create(x.shape());
-    xContrastive.putRow(0, x.getRow(1));
-    xContrastive.putRow(1, x.getRow(2));
-    xContrastive.putRow(2, x.getRow(3));
-    xContrastive.putRow(3, x.getRow(0));
+
+    long numRows = preOutput.shape()[0];
+    
+    for (long i = 1; i <= numRows * kNegativeExamples; i++) {
+      yContrastive.putRow(i - 1, y.getRow(i % numRows));
+      xContrastive.putRow(i - 1, x.getRow(i % numRows));
+    }
+
 
     INDArray xNeighbor = Nd4j.create(x.shape());
     xNeighbor.putRow(0, x.getRow(1));
@@ -96,9 +105,10 @@ public class StructurePreservingEmbeddingLoss implements ILossFunction {
     INDArray structureX = distancesXXNeighbor.add(margin).sub(distancesXXNotANeighbor);
     INDArray structureY = distancesYYNeighbor.add(margin).sub(distancesYYNotANeighbor);
     joinTerm1 = Transforms.max(joinTerm1, 0);
-    joinTerm2 = Transforms.max(joinTerm2, 0);
-    structureX = Transforms.max(structureX, 0);
-    structureY = Transforms.max(structureY, 0);
+    joinTerm2 = Transforms.max(joinTerm2, 0).mul(joinTerm2Weight);
+    structureX = Transforms.max(structureX, 0).mul(structureConstraintXWeight);
+
+    structureY = Transforms.max(structureY, 0).mul(structureConstrainYWeight);
     scoreArr = joinTerm1.add(joinTerm2).add(structureX).add(structureY);
 
 
