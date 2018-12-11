351beab64e765d862ba2993b3ba29dd2129d8cfd
Sebastian Arnold <sarnold@beuth-hochschule.de>
2018-12-11 13:37:46 +0100

    Updated HeadingTag Factory for better performance

--- texoo-sector/src/main/java/de/datexis/sector/encoder/HeadingTag.java
+++ texoo-sector/src/main/java/de/datexis/sector/encoder/HeadingTag.java
@@ -7,7 +7,7 @@ import de.datexis.model.Document;
 import de.datexis.model.Sentence;
 import de.datexis.model.tag.Tag;
 import de.datexis.sector.model.SectionAnnotation;
-import java.io.IOException;
+import java.util.Iterator;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.slf4j.Logger;
@@ -78,14 +78,33 @@ public class HeadingTag implements Tag {
     }
     
     public void attachFromSectionAnnotations(Document doc, Annotation.Source source) {
-      for(SectionAnnotation ann : doc.getAnnotations(source, SectionAnnotation.class)) {
-        String title = ann.getSectionHeading();
-        INDArray vec = encoder.encode(title);
-        doc.streamSentencesInRange(ann.getBegin(), ann.getEnd(), false).forEach(
-          s -> s.putTag(source, new HeadingTag(title, vec))
-        );
+      
+      Iterator<SectionAnnotation> sections = doc.streamAnnotations(source, SectionAnnotation.class).sorted().iterator();
+      
+      if(doc.countSentences() <= 0) return;
+      if(!sections.hasNext()) throw new IllegalArgumentException("Document has no sections");
+      
+      SectionAnnotation ann = sections.next();
+      String title = ann.getSectionHeading();
+      INDArray vec = encoder.encode(title);  
+      HeadingTag tag = new HeadingTag(title, vec);
+      
+      for(Sentence s : doc.getSentences()) {
+        while(s.getBegin() >= ann.getEnd()) {
+          if(sections.hasNext()) {
+            ann = sections.next();
+            title = ann.getSectionHeading();
+            vec = encoder.encode(title);
+            tag = new HeadingTag(title, vec);
+          } else {
+            log.error("Found Document with missing SectionAnnotations for Sentence position {}", s.getBegin());
+          }
+        }
+        s.putTag(source, tag);
       }
+      
       doc.setTagAvailable(source, HeadingTag.class, true);
+      
     }
     
     public void attachFromSentenceVectors(Document doc, Class<? extends Encoder> encoder, Annotation.Source source) {
