635c21b6a13128417dfde88be85ed4eca790c90d
Sebastian Arnold <sarnold@beuth-hochschule.de>
2019-03-14 16:22:22 +0100

    Removed unessesary Object Mapper subtype

--- texoo-sector/src/main/java/de/datexis/sector/reader/WikiSectionReader.java
+++ texoo-sector/src/main/java/de/datexis/sector/reader/WikiSectionReader.java
@@ -1,19 +1,19 @@
 package de.datexis.sector.reader;
 
-import de.datexis.sector.model.WikiDocument;
 import de.datexis.common.ObjectSerializer;
 import de.datexis.common.Resource;
 import de.datexis.model.Annotation;
 import de.datexis.model.Dataset;
 import de.datexis.model.Document;
 import de.datexis.reader.DatasetReader;
-import de.datexis.sector.model.SectionAnnotation;
+import de.datexis.sector.model.WikiDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Reads WikiSection Datasets from JSON file.
@@ -30,7 +30,6 @@ public class WikiSectionReader implements DatasetReader {
   
   public static Dataset readDatasetFromJSON(Resource path) throws IOException {
     log.info("Reading Wiki Articles from {}", path.toString());
-    ObjectSerializer.getObjectMapper().registerSubtypes(SectionAnnotation.class);
     Dataset result = new Dataset(path.getFileName().replace(".json", ""));
     Iterator<Document> it = ObjectSerializer.readJSONDocumentIterable(path);
     while(it.hasNext()) {
@@ -46,7 +45,6 @@ public class WikiSectionReader implements DatasetReader {
   
   public static List<WikiDocument> readWikiDocumentsFromJSON(Resource path) throws IOException {
     log.info("Reading Wiki Articles from {}", path.toString());
-    ObjectSerializer.getObjectMapper().registerSubtypes(SectionAnnotation.class);
     List<WikiDocument> result = new ArrayList<>();
     Iterator<WikiDocument> it = ObjectSerializer.getObjectMapper().readerFor(WikiDocument.class).readValues(path.getInputStream());
     while(it.hasNext()) {
